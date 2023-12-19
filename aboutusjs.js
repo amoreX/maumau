205646178edb0c7910a5a75bc57b81c723b2808a
amor3x <71655709+amoreX@users.noreply.github.com>
2023-12-19 22:22:17 +0530

    Update aboutusjs.js

--- aboutusjs.js
+++ aboutusjs.js
@@ -1,9 +1,7 @@
-gsap.registerPlugin(ScrambleTextPlugin);
+
 
 $(document).ready(function() {
-    gsap.to("#idk", {duration: 3.6, scrambleText: "Wanna know more about us?Here you go ฅ^･ω･^ฅ"});
-    gsap.to("#idk", {opacity:0,duration:0.2,delay:3.6,onComplete:niggawhat});
-    gsap.to(".iidk", {opacity:1,duration:0.2,delay:3.8});
+    
     $("#amorexpfp").on("mouseover", function() {
         $(".tnihal").addClass("visible");
         $(".amorecardcontent").addClass("visiblecard");
@@ -23,9 +21,7 @@ $(document).ready(function() {
         $(".tasad").removeClass("visible");
         $(".asadcardcontent").removeClass("visiblecard");
     });
-    function niggawhat(){
-        $("#idk").style.display="none";
-    }
+   
         
     
 
