9ba35f27a588ed5771fe6880fd570929e4f890d2
Md Nihal Rahman <rahmannihal72@gmail.com>
2023-12-19 21:45:17 +0530

    added ainmation

--- aboutusjs.js
+++ aboutusjs.js
@@ -1,27 +1,33 @@
+gsap.registerPlugin(ScrambleTextPlugin);
+
 $(document).ready(function() {
+    gsap.to("#idk", {duration: 3.6, scrambleText: "Wanna know more about us?Here you go ฅ^･ω･^ฅ"});
+    gsap.to("#idk", {opacity:0,duration:0.2,delay:3.6,onComplete:niggawhat});
+    gsap.to(".iidk", {opacity:1,duration:0.2,delay:3.8});
     $("#amorexpfp").on("mouseover", function() {
         $(".tnihal").addClass("visible");
         $(".amorecardcontent").addClass("visiblecard");
     });
-
+    
     $("#amorexpfp").on("mouseout", function() {
         $(".tnihal").removeClass("visible");
         $(".amorecardcontent").removeClass("visiblecard");
     });
-
+    
     $("#asadpfp").on("mouseover", function() {
         $(".tasad").addClass("visible");
         $(".asadcardcontent").addClass("visiblecard");
     });
-
+    
     $("#asadpfp").on("mouseout", function() {
         $(".tasad").removeClass("visible");
         $(".asadcardcontent").removeClass("visiblecard");
     });
-    window.addEventListener('load', (event) => {
+    function niggawhat(){
+        $("#idk").style.display="none";
+    }
+        
     
-        document.body.style.opacity = '1';
-    });
 
 });
 
